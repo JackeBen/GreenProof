e9154dad3e1b8bb986d7921aee404488443a31ea
IsabelWolf <thanhthaohuynh545@gmail.com>
2025-10-02 19:03:55 +0800

    Base configs

---
+++ frontend/next.config.ts
@@ -0,0 +1,12 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    esmExternals: true,
+  },
+};
+
+export default nextConfig;
+
+
